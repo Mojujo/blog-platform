18ca23f78e5e5228545f7788e139e363d2d79203
Mojujo <oscar.tornqvist01@gmail.com>
2025-11-20 00:20:44 +0100

    setup

---
+++ services/notification-service/src/test/java/se/mojujo/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.mojujo.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
