896874ca6df136c2c1218eaefcff6e04c8d3826f
Mojujo <oscar.tornqvist01@gmail.com>
2025-11-28 07:05:43 +0100

    refactor: removed unused import delete: removed ununsed role repository

--- services/user-service/src/main/java/se/mojujo/userservice/exception/EmailAlreadyExistsException.java
+++ services/user-service/src/main/java/se/mojujo/userservice/exception/EmailAlreadyExistsException.java
@@ -1,10 +1,6 @@
 package se.mojujo.userservice.exception;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ProblemDetail;
-import org.springframework.web.ErrorResponse;
 
 public class EmailAlreadyExistsException extends ErrorResponseFormat {
     public EmailAlreadyExistsException(String message) {
--- services/user-service/src/main/java/se/mojujo/userservice/exception/UsernameAlreadyExistsException.java
+++ services/user-service/src/main/java/se/mojujo/userservice/exception/UsernameAlreadyExistsException.java
@@ -1,10 +1,6 @@
 package se.mojujo.userservice.exception;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ProblemDetail;
-import org.springframework.web.ErrorResponse;
 
 public class UsernameAlreadyExistsException extends ErrorResponseFormat {
     public UsernameAlreadyExistsException(String message) {
