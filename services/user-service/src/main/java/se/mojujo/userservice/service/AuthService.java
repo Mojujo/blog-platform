5d6ab22ea2a4f2aec3903c174998793496db1be6
Mojujo <oscar.tornqvist01@gmail.com>
2025-12-03 21:26:29 +0100

    refactor: removed unused import

--- services/user-service/src/main/java/se/mojujo/userservice/service/AuthService.java
+++ services/user-service/src/main/java/se/mojujo/userservice/service/AuthService.java
@@ -9,7 +9,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import se.mojujo.userservice.exception.AuthorizationExpired;
 import se.mojujo.userservice.exception.InvalidCredentialsException;
