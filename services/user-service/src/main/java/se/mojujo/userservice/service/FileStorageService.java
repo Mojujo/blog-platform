6837ae16b536d77a12a970caeffec76312d4929f
Mojujo <oscar.tornqvist01@gmail.com>
2026-01-06 02:42:02 +0100

    refactor: removed unused import

--- services/user-service/src/main/java/se/mojujo/userservice/service/FileStorageService.java
+++ services/user-service/src/main/java/se/mojujo/userservice/service/FileStorageService.java
@@ -4,7 +4,6 @@ import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import se.mojujo.userservice.exception.FileStorageException;
